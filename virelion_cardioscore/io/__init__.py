ffaab77b58f4285bf6a3f8df670d3789ea41b89c
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-16 20:42:59 +0300

    Add core package: config, io, reporting, CI, example config

---
+++ virelion_cardioscore/io/__init__.py
@@ -0,0 +1,13 @@
+"""Input/output utilities for MEA data and results."""
+
+from virelion_cardioscore.io.synthetic import (
+    generate_synthetic_mea,
+    load_synthetic_dataset,
+    SyntheticMEADataset,
+)
+
+__all__ = [
+    "generate_synthetic_mea",
+    "load_synthetic_dataset",
+    "SyntheticMEADataset",
+]
---
+++ virelion_cardioscore/reporting/__init__.py
@@ -0,0 +1,5 @@
+"""Report generation utilities."""
+
+from virelion_cardioscore.reporting.report_generator import write_html_report
+
+__all__ = ["write_html_report"]
