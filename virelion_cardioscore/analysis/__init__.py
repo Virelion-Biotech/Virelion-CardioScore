32e032cf73fd21080d740e4928abbc88fe6219a2
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-17 03:36:04 +0300

    feat: expose dose-response fitting API

--- virelion_cardioscore/analysis/__init__.py
+++ virelion_cardioscore/analysis/__init__.py
@@ -1,11 +1,21 @@
-"""Analysis modules: concentration-response and CardioScore engine."""
+"""Analysis modules: dose-response fitting and CardioScore engine."""
 
 from virelion_cardioscore.analysis.cipa_scoring import CardioScoreEngine, ScoreResult
+from virelion_cardioscore.analysis.dose_response import (
+    DoseResponseFit,
+    fit_4pl,
+    fit_concentration_series,
+    four_parameter_logistic,
+)
 from virelion_cardioscore.analysis.pipeline import CardioScorePipeline, PipelineResult
 
 __all__ = [
     "CardioScoreEngine",
     "ScoreResult",
+    "DoseResponseFit",
+    "fit_4pl",
+    "fit_concentration_series",
+    "four_parameter_logistic",
     "CardioScorePipeline",
     "PipelineResult",
 ]
